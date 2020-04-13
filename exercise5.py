ba030da6e9c9c32bf426ee752b5bc7149a2871ee
Guilherme Maciel <ghm.guilherme@gmail.com>
2020-04-13 22:23:32 +0200

    Adding exercise5.py file

--- exercise5.py
+++ exercise5.py
@@ -1,12 +1,14 @@
  # Set the message variable equal to any string containing a new-line escape sequence
 message = "This is a new \n line"
 
-
 # Add a string to the mountains variable that when printed results in: /\/\/\
 # You will need to use an escape sequence more than once!
-mountains = "Table Mountain isn't like this \/\\\/\\\/\\"
-
+mountains = "Table Mountain isn't like this /\\/\\/\\"
 
 # Set the quotation variable to any string that contains an escaped double quotation mark
 
 quotation = "Let\'s try \"that\""
+
+print(message)
+print(mountains)
+print(quotation)
