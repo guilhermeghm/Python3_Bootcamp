0327467bd825144ab30e4d7dee909309f226e108
Guilherme Maciel <ghm.guilherme@gmail.com>
2020-06-14 16:53:55 +0200

    End of the Section 26

--- grumpy.py
+++ grumpy.py
@@ -6,6 +6,17 @@ class GrumpyDict(dict):
     def __missing__(self, key):
         print(f"You want {key}? Well it ain't here!")
 
+    def __setitem__(self, key, value):
+        print("You want to change the Dictonary?")
+        print("Ok Fine...")
+        super().__setitem__(key, value)
+
+    def __contains__(self, item):
+        print("No it ain't in here!")
+        return False
+
 data = GrumpyDict({"first": "Tom", "animal": "cat"})
 print(data)
-data['city']
+data['city'] = 'Tokyo'
+print(data)
+'city' in data
