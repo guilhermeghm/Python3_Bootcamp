a096860c39a820aa32ee0f883bab6c6dabf0e751
Guilherme Maciel <ghm.guilherme@gmail.com>
2020-06-27 17:53:00 +0200

    Section 36 - Part 1

---
+++ exercise108.py
@@ -0,0 +1,10 @@
+'''
+reverse_string('awesome') # 'emosewa'
+reverse_string('Colt') # 'tloC'
+reverse_string('Elie') # 'eilE'
+'''
+
+# add whatever parameters you deem necessary - good luck!
+def reverse_string(value):
+    # implement your function here
+    return value[::-1]
