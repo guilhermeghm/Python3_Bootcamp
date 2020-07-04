1db14375bfe2a71b882409b8811c3558e3fbf3e8
Guilherme Maciel <ghm.guilherme@gmail.com>
2020-07-04 15:34:37 +0200

    Section 36 - Part 3

---
+++ exercise122.py
@@ -0,0 +1,16 @@
+'''
+find_the_duplicate([1,2,1,4,3,12]) # 1
+find_the_duplicate([6,1,9,5,3,4,9]) # 9
+find_the_duplicate([2,1,3,4]) # None
+'''
+
+def find_the_duplicate(arr):
+    counter = {}
+    for val in arr:
+        if val in counter:
+            counter[val] += 1
+        else:
+            counter[val] = 1
+    for key in counter.keys():
+        if counter[key] > 1:
+            return int(key)
